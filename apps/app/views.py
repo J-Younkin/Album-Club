3b6ec90a37a2a50de11471abe258ac8ff36a4b0a
J-Younkin <younkmail@gmail.com>
2020-02-19 22:30:14 -0800

    added icons, log/reg templates, routing

--- apps/app/views.py
+++ apps/app/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request, 'app/index.html')
+
+def login_form(request):
+    return render(request, "app/sign_in.html")
+
+def create_form(request):
+    return render(request, 'app/create_club.html')
+
+    
